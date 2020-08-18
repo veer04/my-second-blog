6c4aa431e70c4b150f71f281a66c46b6c77f651c
Jarwis <681jarwis@gmail.com>
2020-08-18 22:43:35 +0530

    Adding security and login page .

--- blog/views.py
+++ blog/views.py
@@ -3,22 +3,28 @@ from .models import Post
 from django.utils import timezone
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def post_list(request):
 	#return render(request,'blog/post_list.html',{})
-	#posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
-	posts = Post.objects.all()
+	posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('-published_date')
+	#posts = Post.objects.all()
 	return render (request, 'blog/post_list.html',{'posts':posts})
+
+@login_required
 def post_detail(request, pk):
 	post=get_object_or_404(Post, pk=pk)
 	return render(request,'blog/post_detail.html',{'post':post})
+
+@login_required
 def post_new(request):
 	if request.method == "POST":
 		form = PostForm(request.POST)
 		if form.is_valid():
 			post = form.save(commit=False)
 			post.author = request.user
-			post.published_date = timezone.now()
+			#post.published_date = timezone.now()
 			post.save()
 			return redirect('post_detail', pk=post.pk)
 
@@ -26,6 +32,7 @@ def post_new(request):
 		form=PostForm()
 	return render(request,'blog/post_edit.html',{'form':form})
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -33,11 +40,27 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            #post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
+def post_draft_list(request):
+	posts = Post.objects.filter(published_date__isnull=True).order_by('-created_date')
+	return render(request, 'blog/post_draft_list.html',{'posts':posts})
+
+@login_required
+def post_publish(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	post.publish()
+	return redirect('post_detail', pk=pk)
+
+@login_required
+def post_remove(request, pk):
+	post = get_object_or_404(Post, pk =pk )
+	post.delete()
+	return redirect('post_list')
 # Create your views here.
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,10 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
+#from django.contrib.auth.models import Permission, User
+from django.contrib.auth.views import LoginView, LogoutView
+
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/login/',views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('blog.urls')),
+
+
 ]
 
 
